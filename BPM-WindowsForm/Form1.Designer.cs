a211e0b21b92621d8c958b614ffe5090a91079ce
Guy Templeton <guyjtempleton@googlemail.com>
2015-04-26 08:29:48 +0100

    Increase max value of noOfRuns to 100k

--- BPM-WindowsForm/Form1.Designer.cs
+++ BPM-WindowsForm/Form1.Designer.cs
@@ -100,6 +100,11 @@
             // numericUpDown2
             // 
             this.numericUpDown2.Location = new System.Drawing.Point(152, 63);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown2.TabIndex = 5;
